999b75405ba8f2671608b8b4949d7a701357bede
Darafei A. Praliaskouski <me@komzpa.net>
2016-12-25 15:33:49 +0300

    debug info

--- feed_measurements.py
+++ feed_measurements.py
@@ -5,3 +5,6 @@ def feed_measurement(data):
     url = "http://api.openweathermap.org/data/3.0/measurements?appid=%s" % config.apikey
     req = requests.post(url, json=data)
     print(req)
+    print(req.status_code)
+    print(req.headers)
+    print(req.raw)
